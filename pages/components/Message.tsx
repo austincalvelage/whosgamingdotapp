d33fa970c08a4f54b9de170df4507b470e920e21
Austin Calvelage <austin.calvelage@icloud.com>
2022-12-03 22:59:46 -0500

    Story Component WIP, React-icons install

---
+++ pages/components/Message.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from 'next'
+import { motion } from 'framer-motion'
+import classNames from 'classnames'
+
+interface Props {
+  time?: string
+  text: string
+  response: boolean
+  animate?: object
+  transition?: object
+  initial?: object
+}
+
+const Message: NextPage<Props> = ({ time, text, response, animate, transition, initial }) => {
+  return (
+    <motion.div initial={initial} className='flex flex-col space-y-2' animate={animate} transition={transition}>
+      <span className='self-center text-xs text-[#8A8A8D]'>{time}</span>
+      <p
+        className={classNames(
+          'w-fit max-w-[65%] rounded-lg p-2 text-xs shadow-md',
+          response ? 'self-end bg-[#6ACE6B] text-white' : 'self-start bg-[#E9E9EB] text-black'
+        )}
+      >
+        {text}
+      </p>
+    </motion.div>
+  )
+}
+
+export default Message
