82f4670bda22c2b2e82fe29353fde9e12c2945cd
Austin Calvelage <austin.calvelage@icloud.com>
2022-12-08 20:13:06 -0500

    Revert "Dev"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,3 @@
-import Story from './components/Story'
-
 export default function Home() {
-  return (
-    <div className='flex h-screen items-center justify-center'>
-      <Story />
-    </div>
-  )
+  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
 }
